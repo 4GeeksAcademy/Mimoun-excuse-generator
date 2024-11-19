344ac4c409e9299a2e715ed34e436158ce2216f3
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2024-11-19 20:04:17 +0000

    revision: style code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+
 window.onload = function() {
   let first = "Un ";
   let noun = [
@@ -15,7 +16,7 @@ window.onload = function() {
     "toro ",
     "pájaro "
   ];
-  let adj = [
+  let adjective = [
     "maloliente ",
     "amargado ",
     "verde ",
@@ -31,7 +32,7 @@ window.onload = function() {
     "robó ",
     "quemó "
   ];
-  let possetion = [
+  let possession = [
     "mi zapato ",
     "mi moto ",
     "mi sobrino ",
@@ -50,18 +51,17 @@ window.onload = function() {
     "en la estacion de tren"
   ];
 
-  let rdm1 = Math.floor(Math.random() * noun.length);
-  let rdm2 = Math.floor(Math.random() * adj.length);
-  let rdm3 = Math.floor(Math.random() * action.length);
-  let rdm4 = Math.floor(Math.random() * possetion.length);
-  let rdm5 = Math.floor(Math.random() * where.length);
-
-  // creating a sentence (the excuse)
+  let indexNoun = Math.floor(Math.random() * noun.length);
+  let indexAdjective = Math.floor(Math.random() * adjective.length);
+  let indexAction = Math.floor(Math.random() * action.length);
+  let indexPossession = Math.floor(Math.random() * possession.length);
+  let indexWhere = Math.floor(Math.random() * where.length);
+  
   document.querySelector("#excuse").innerHTML =
     first +
-    noun[rdm1] +
-    adj[rdm2] +
-    action[rdm3] +
-    possetion[rdm4] +
-    where[rdm5];
+    noun[indexNoun] +
+    adjective[indexAdjective] +
+    action[indexAction] +
+    possession[indexPossession] +
+    where[indexWhere];
 };
